6a76210f99c70cd6851776897948fbcfea96642c
sdierauf <sdierauf@gmail.com>
2014-07-23 13:21:14 -0700

    finished dispatcher

---
+++ js/dispatcher/Dispatcher.js
@@ -0,0 +1,51 @@
+var Promise = require('es6-promise').Promise;
+var merge = require('react/lib/merge');
+
+
+var _callbacks = [];
+var _promises = [];
+
+var _addPromise = function(callback, payload) {
+    _promises.push(new Promise(function(resolve, reject) {
+        if (callback(payload)) {
+            resolve(payload);
+        } else {
+            reject(new Error('Dispatcher callback unsuccessful'));
+        }
+    }));
+};
+
+var Dispatcher = function() {};
+Dispatcher.prototype = merge(Dispatcher.prototype, {
+
+    register: function(callback) {
+        _callbacks.push(callback);
+        return _callbacks.length - 1; //the index
+    },
+
+    dispatch: function(payload) {
+        //create an array of promises for callbacks to reference.
+        var resolves = [];
+        var rejects = [];
+
+        _promises = _callbacks.map(function(_, i) {
+            return new Promise(function(resolve, reject) {
+                resolves[i] = resolve;
+                rejects[i] = reject; 
+            });
+        });
+
+        //dispatch to callbacks and resolbe/reject promises.
+        _callbacks.forEach(function(callback, i) {
+            //calback can return obj to resolve or a promise to chain
+            Promise.resolve(callback(payload)).then(function() {
+                resolves[i](payload);
+            }, function() {
+                rejects[i](new Error('Dispatcher callback unsuccessful'));
+            });
+        });
+        _promises = [];
+    }
+});
+
+module.exports = Dispatcher;
