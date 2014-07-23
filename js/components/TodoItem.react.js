0e688631e70eab5a9b93d11786909dd5e9f6aa2a
sdierauf <sdierauf@gmail.com>
2014-07-23 14:19:18 -0700

    finished more stuff

---
+++ js/components/TodoItem.react.js
@@ -0,0 +1,34 @@
+/** @jsx React.DOM */
+
+var React = require('react');
+var TodoActions = require('../actions/TodoActions');
+var TodoTextInput = require('./TodoTextInput.react');
+
+var TodoItem = React.createClass({
+
+    propTypes: {
+        todo: React.PropTypes.object.isRequired
+    },
+
+
+    render: function() {
+        var todo = this.props.todo;
+
+        return (
+            <li
+                key={todo.id}>
+                <label>
+                    {todo.text}
+                </label>
+                <button className="destroy" onClick={this._onDestroyClick} />
+            </li>
+        );
+    },
+
+    _onDestroyClick: function() {
+        TodoActions.destroy(this.props.todo.id);
+    }
+
+});
+
+module.exports = TodoItem;
